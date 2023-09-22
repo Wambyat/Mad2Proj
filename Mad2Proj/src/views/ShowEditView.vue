f8c546f2602394aecdb7055938411e715695b962
Anirudha Anekal <77496170+Wambyat@users.noreply.github.com>
2023-09-22 23:12:56 +0530

    Update ShowEditView.vue

--- Mad2Proj/src/views/ShowEditView.vue
+++ Mad2Proj/src/views/ShowEditView.vue
@@ -4,19 +4,11 @@
         <p>Date: <input :value="dets[2]" type="date" /></p>
         <p>Venue ID: <input :value="dets[3]" /></p>
         <p>Seats: <input :value="dets[4]" /></p>
-        <p>Details: <span
-            @input="(e) => (state.input = e.target.innerText)"
-            contenteditable
-            >{{ state.input }}</span
-        ></p>
-    </div>
+        <p>Details: <input :value="dets[6]" /></p>
 </template>
 <script>
-    import { ref,reactive } from "vue";
+    import { ref } from "vue";
     import axios from "axios";
-    const state = reactive({
-        input: "My width will adapt to the text",
-    });
     export default {
         data() {
             return {
@@ -35,10 +27,9 @@
                 .then((responce) => {
                     const res = responce.data;
                     dets.value = res["data"][0];
-                    state.input = dets.value[6];
                 });
 
-            return { dets,state };
+            return { dets };
         },
     };
 </script>
